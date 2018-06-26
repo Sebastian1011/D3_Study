3a4debb722150f4ac2ea21af5670c530f0a58ee8
zmzhang <ziming.zhang@yitu-inc.com>
2018-06-26 14:44:47 +0800

    data demo

--- data_demo/index.js
+++ data_demo/index.js
@@ -20,10 +20,48 @@ function render(data){
         .remove();
 }
 
-setInterval(function(){
-    data.shift();
-    data.push(Math.floor(Math.random() * 100));
-    render(data);
-}, 1500);
-render(data)
+function runDataDemo() {
+    setInterval(function () {
+        data.shift();
+        data.push(Math.floor(Math.random() * 100));
+        render(data);
+    }, 1500);
+    render(data)
+}
 
+// runDataDemo();
+
+var fnData = [];
+var next = function next(x) {
+    return 15 + x*x; 
+}
+var newData = function newData(){
+    fnData.push(next);
+    return fnData;
+}
+function renderFnData(){
+    var selection = d3.select('#container')
+        .selectAll('div')
+        .data(newData);
+    selection.enter()
+        .append("div")
+        .append("span")
+    selection.exit()
+        .remove();
+    selection.attr("class", "v-bar")
+        .style("height", function(d, i){
+            return d(i) + "px";
+        })
+        .select("span")
+        .text(function(d, i){
+            return d(i);
+        })
+}
+
+function runFnData(){
+    setInterval(function () {
+        renderFnData()
+    }, 1500);
+    renderFnData();
+}
+runFnData();
--- dev-server.js
+++ dev-server.js
@@ -3,10 +3,10 @@ const liveServer = require("live-server");
 const params = {
     port: 8181,
     host: "0.0.0.0",
-    root: "./",
     file: "index.html",
     open: false,
-    wait: 1000,
+    ignore: "node_modules",
+    wait: 100,
     logLevel: 2,
     middleware: [function(req, res, next){next();}]
 }
